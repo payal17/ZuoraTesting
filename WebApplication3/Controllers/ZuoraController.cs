3963bd9e3b29442f3944773ceeb49ca9e4373185
Payal Goel <Payal.Goel@unisys.com>
2020-07-16 13:04:49 +0530

    Testing

---
+++ WebApplication3/Controllers/ZuoraController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ZuoraController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/Zuora/GetDetails")]
+        public ZuoraModel GetDetails()
+        {
+            ZuoraModel z = new ZuoraModel();
+            z.message = "message came";
+            z.success = true;
+            return z;
+        }
+    }
+}
---
+++ WebApplication3/Models/ZuoraModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class ZuoraModel
+    {
+        public string message
+        {
+            get;
+            set;
+        }
+
+        public Boolean  success
+        {
+            get;
+            set;
+        }
+
+    }
+}
